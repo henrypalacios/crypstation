547d9c6754bedadb95333a0ff52686b419b98667
Henry Palacios <henrydpalacios@gmail.com>
2019-06-10 22:30:12 -0300

    fix root_dir

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,6 +4,6 @@ DEBUG = False
 
 #  Indicate where the static files should be placed.
 #  This is necessary so that Nginx can handle requests for these items
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(ROOT_DIR, 'static/')
 
 ALLOWED_HOSTS = ['.example.com']
