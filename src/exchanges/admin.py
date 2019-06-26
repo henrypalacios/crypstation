e62558ba6470c0ac9c066ee266a9d76503722c60
Henry Palacios <henrydpalacios@gmail.com>
2019-06-25 22:36:10 -0300

    PasswordInput in admin

--- src/exchanges/admin.py
+++ src/exchanges/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
+from django.forms import ModelForm, PasswordInput
 
 from .models import Account
 
 
+class AccountForm(ModelForm):
+    class Meta:
+        model = Account
+        fields = ['apiKey', 'secret', 'password', 'uid', 'exchange']
+        widgets = {
+            'apiKey': PasswordInput(),
+            'secret': PasswordInput(),
+            'password': PasswordInput(),
+        }
+
+
 @admin.register(Account)
 class AccountAdmin(admin.ModelAdmin):
+    form = AccountForm
     list_display = ('pk', 'uid', 'exchange')
